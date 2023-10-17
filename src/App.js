02a7de82096d50dae568287939e1280778d97351
Jp Keys <jpkeys@jps-macbook-pro.myfiosgateway.com>
2023-10-17 14:55:00 -0400

    items mapping

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
+const initialItems = [
+  { id: 1, description: "Passports", quantity: 2, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: false },
+];
+
 export default function App() {
   return (
-    <div>
+    <div className="app">
       <Logo />
       <Form />
       <PackingList />
@@ -18,8 +23,15 @@ function Form() {
   </div>
 }
 function PackingList() {
-  return <div className="list">My List</div>
+  return <ul className="list">
+    {initialItems.map(item => <Item item={item} />)}
+  </ul>
 }
+
+function Item({item}) {
+  return <li>{item.description}</li>
+}
+
 function Stats() {
   return (
     <footer className="stats">
