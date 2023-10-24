89028c5c6fa8b9158e75d94a9adf8ef83dcc3cff
Jp Keys <jpkeys@jps-macbook-pro.myfiosgateway.com>
2023-10-24 14:30:41 -0400

    getting data from the form

--- src/App.js
+++ src/App.js
@@ -24,17 +24,20 @@ function Logo() {
 
 function Form() {
   const [description, setDescription] = useState('');
-  const [quantity, setQunatity] = useState(5);
+  const [quantity, setQuantity] = useState(1);
 
   function handleSubmit(e){
     e.preventDefault();
-    // setItem((currentItem) => currentItem + 1);
-  }  
+
+    const newItem = { description, quantity, packed: false, id: Date.now()};
+    console.log(newItem);
+    }
+
 
   return (
     <form className="add-form" onSubmit={handleSubmit}>
       <h3>What do I need for my trip? 🧐</h3>
-      <select value={quantity} onChange={e => setQunatity(Number(e.target.value))}>
+      <select value={quantity} onChange={e => setQuantity(Number(e.target.value))}>
         {Array.from({ length : 20}, (_, i) => i + 1).map((num) => (
           <option value={num} key={num}>
             {num}
