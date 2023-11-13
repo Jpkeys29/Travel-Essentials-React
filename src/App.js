3f31bd4ad09b300fb17fd04ba6925f9fa5e615ea
Jp Keys <jpkeys@jps-macbook-pro.myfiosgateway.com>
2023-11-13 15:29:49 -0500

    Moving components into separate files 1

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import Logo from './Logo';
+import Form from "./Form";
+import PackingList from "./PackingList";
 
 
 export default function App() {
@@ -34,87 +37,6 @@ export default function App() {
       <Stats items={items}/>
     </div>
   );
-}
-
-function Logo() {
-  return <h1> Travel Essentials ✈️</h1>
-}
-
-
-function Form({onAddItems}) {
-  const [description, setDescription] = useState('');
-  const [quantity, setQuantity] = useState(1);
-
-
-  function handleSubmit(e){
-    e.preventDefault();
-
-    if (!description) return;
-
-    const newItem = { description, quantity, packed: false, id: Date.now()};
-    // console.log(newItem);
-
-    onAddItems(newItem);
-
-    setDescription("");
-    setQuantity(1);
-
-    }
-
-
-  return (
-    <form className="add-form" onSubmit={handleSubmit}>
-      <h3>What do I need for my trip? 🧐</h3>
-      <select value={quantity} onChange={e => setQuantity(Number(e.target.value))}>
-        {Array.from({ length : 20}, (_, i) => i + 1).map((num) => (
-          <option value={num} key={num}>
-            {num}
-          </option>
-        ))}     
-      </select>
-      <input type="text" placeholder="Item" 
-      value={description} onChange={(e) => setDescription(e.target.value)}/>
-      <button onClick={handleSubmit}>Add</button>
-    </form>
-  );  
-}
-
-function PackingList({items, onDeleteItem, onCheckItem, onClearList}) {
-  const [sortBy, setSortBy] = useState("input");
-
-  let sortedItems;
-  if(sortBy === 'input') sortedItems = items;
-  if(sortBy === 'description') sortedItems = items.slice().sort((a, b) => a.description.localeCompare(b.description));
-  if(sortBy === 'packed') sortedItems = items; items.slice().sort((a,b) => Number(a.packed) - Number(b.packed));
-
-
-  return (
-    <div className="list">
-      <ul>
-        {sortedItems.map((item) => <Item item={item} onDeleteItem={onDeleteItem} onCheckItem={onCheckItem} key={item.id}/>)}
-      </ul>
-      <div className="actions">
-        <select value={sortBy} onChange={(e) => setSortBy(e.target.value)}>
-          <option value="input">Sort by input order</option>
-          <option value="description">Sort by description</option>
-          <option value="packed">Sort by packed status</option>
-        </select>
-        <button onClick={onClearList}>Clear list</button>
-      </div>
-    </div>
-  );
-}
-
-function Item({item, onDeleteItem, onCheckItem}) {
-  return (
-    <li>
-      <input type="checkbox" value={item.packed} onChange={() => onCheckItem(item.id)}/>
-      <span style={item.packed ? {textDecoration:"line-through"} : {}}>
-        {item.quantity} {item.description}
-      </span>
-      <button onClick={() => onDeleteItem(item.id)}>🪓</button>
-    </li>
-  ) 
 }
 
 function Stats({items}) {
